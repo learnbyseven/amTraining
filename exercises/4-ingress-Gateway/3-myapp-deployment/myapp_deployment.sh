f252794749ea574effc7782e00bc3161e226a76c
learnbyseven <34051943+learnbyseven@users.noreply.github.com>
2023-02-14 15:14:44 +1100

    Update myapp_deployment.sh

--- exercises/4-ingress-Gateway/3-myapp-deployment/myapp_deployment.sh
+++ exercises/4-ingress-Gateway/3-myapp-deployment/myapp_deployment.sh
@@ -2,6 +2,6 @@
 kubectl create ns myapp
 kubectl label ns myapp istio-injection=enabled
 kubectl get namespace -L istio-injection
-kubectl -n myapp create secret tls myapp-certs --key exercises/ex4-ingress-Gateway/certs/server.key --cert exercises/ex4-ingress-Gateway/certs/server.crt
-kubectl -n myapp create configmap myapp-configmap --from-file=nginx.conf=exercises/ex4-ingress-Gateway/3-myapp-deployment/nginx.conf
-kubectl -n myapp create -f exercises/ex4-ingress-Gateway/3-myapp-deployment/myapp_deployment.yaml
+kubectl -n myapp create secret tls myapp-certs --key exercises/4-ingress-Gateway/certs/server.key --cert exercises/4-ingress-Gateway/certs/server.crt
+kubectl -n myapp create configmap myapp-configmap --from-file=nginx.conf=exercises/4-ingress-Gateway/3-myapp-deployment/nginx.conf
+kubectl -n myapp create -f exercises/4-ingress-Gateway/3-myapp-deployment/myapp_deployment.yaml
