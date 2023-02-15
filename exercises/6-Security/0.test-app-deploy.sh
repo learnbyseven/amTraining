0a17cade6c958a4f0fba4606464e38ded2fc04ef
learnbyseven <34051943+learnbyseven@users.noreply.github.com>
2023-02-15 14:10:29 +1100

    Update 0.test-app-deploy.sh

--- exercises/6-Security/0.test-app-deploy.sh
+++ exercises/6-Security/0.test-app-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-kubectl create -f https://raw.githubusercontent.com/learnbyseven/amTraining/main/exercises/ex2-sample-apps-install/1-sample-apps-deployment/test.yaml
+kubectl create -f https://raw.githubusercontent.com/learnbyseven/amTraining/main/exercises/2-sample-apps-install/1-sample-apps-deployment/test.yaml
 sleep 1
 echo "Test namespace and app deployed"
 sleep 2
